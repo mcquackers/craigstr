3e767f0de2db0405061b63766ddf6778bfe657eb
Brian McQueen <brian.d.mcqueen@gmail.com>
2014-11-04 16:04:06 -0500

    Adds line-splitting and better require_admin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,8 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def require_admin
-    current_user.admin?
+    unless current_user.admin?
+      redirect_to root_path
+    end
   end
 end
