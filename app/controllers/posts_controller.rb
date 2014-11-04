85326cb9c4aa40d428682f2c01a9123b6a0c2b53
Brian McQueen <brian.d.mcqueen@gmail.com>
2014-11-04 16:13:06 -0500

    Fixed . issues on line-splitting

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -28,9 +28,9 @@ class PostsController < ApplicationController
   private
 
   def post_params
-    params
-      .require(:post)
-      .permit(:title, :body, :spam)
-      .merge(user_id: current_user.id)
+    params.
+      require(:post).
+      permit(:title, :body, :spam).
+      merge(user_id: current_user.id)
   end
 end
