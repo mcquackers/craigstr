3f40059270c5005e27167da14ac84fdd244182e4
Brian McQueen <brian.d.mcqueen@gmail.com>
2014-11-05 10:49:23 -0500

    Removed more whitespace

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   resource :session, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create]
+  resources :spams, only: [:index]
   resources :regions, only: [:create, :show, :index] do
     resources :categories, only: [:new, :create, :show]
     resources :posts, only: [:new, :create, :show, :update, :destroy]
   end
-  resources :spams, only: [:index]
 
   root to: "regions#index"
 end
