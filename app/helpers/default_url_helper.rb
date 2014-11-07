2c75dcb72ea25aa2b34583f8a9af813dc757515b
Brian McQueen <brian.d.mcqueen@gmail.com>
2014-11-07 11:49:28 -0500

    Minor refactor of default_image helper

--- app/helpers/default_url_helper.rb
+++ app/helpers/default_url_helper.rb
@@ -1,10 +1,7 @@
 module DefaultUrlHelper
   DEFAULT_URL = "https://avatars3.githubusercontent.com/u/8669803?v=2&s=400"
+
   def default_image(image_url)
-    if image_url.present?
-      image_url
-    else
-      DEFAULT_URL
-    end
+    image_url.presence || DEFAULT_URL
   end
 end
